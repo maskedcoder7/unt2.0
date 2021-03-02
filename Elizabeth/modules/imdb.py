7fab39ccf927fdf636ac845b7a3e886595cdec75
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-02 21:32:31 +0530

    Update imdb.py

--- Elizabeth/modules/imdb.py
+++ Elizabeth/modules/imdb.py
@@ -36,7 +36,7 @@ async def is_register_admin(chat, user):
 async def imdb(e):
  if e.is_group:
   if not (await is_register_admin(e.input_chat, e.message.sender_id)):
-     await event.reply("😜 Hai.. You are not admin..🤭 You can't use this command.. But you can use in my pm🙈")
+     await event.reply("😁Heya..you are not a admin or mod 🥺 you can't use this command in this chat.. But you can use me only in pm🤗")
      return
  try:
     movie_name = e.pattern_match.group(1)
