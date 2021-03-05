af53cd435fea430b72f29a9b6c65400ed83bbd26
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-05 20:20:58 +0530

    Update __main__.py

--- Elizabeth/__main__.py
+++ Elizabeth/__main__.py
@@ -177,7 +177,7 @@ def start(update, context):
                 disable_web_page_preview=False,
             )
     else:
-        update.effective_message.reply_text("Heya, 乂🖤⃝ƛԼƛƝƘƦƖƬӇƛ⃟🦋࿐ Here, How can I help you? 🙂")
+        update.effective_message.reply_text("Heya,𝓔𝓵𝓲𝔃𝓪𝓫𝓮𝓽𝓱࿐ Here, How can I help you? 🧐")
 
 
 
