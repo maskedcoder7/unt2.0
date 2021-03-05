b4e0b40f06cc8e9f6dd1eb391aa73f10f6b21b6d
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-05 20:15:01 +0530

    Update __main__.py

--- Elizabeth/__main__.py
+++ Elizabeth/__main__.py
@@ -642,7 +642,7 @@ def main():
         updater.start_polling(timeout=15, read_latency=4)
         updater.bot.send_message(
             chat_id=MESSAGE_DUMP,
-            text="Elizabeth Running 🤗...")
+            text="Who do you think you are, Forrest Gump?")
         client.run_until_disconnected()
 
     updater.idle()
